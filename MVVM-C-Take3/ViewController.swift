39f1cf4f4acc0b36f6829f48200e73d38b25304b
Arun Sirrpi <sinthanaisirrpi.arun@abc.net.au>
2023-01-25 19:43:02 +1100

    Initial Commit

---
+++ MVVM-C-Take3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C-Take3
+//
+//  Created by Arun Sinthanaisirrpi on 25/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
